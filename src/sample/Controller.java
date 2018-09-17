7b5f3d8f123aa01fb175363193bf615f8f51dea6
George-E-M <georgeedwinmcerlean@gmail.com>
2018-09-17 17:26:08 +1200

    Testing it only adds to src

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 public class Controller {
     // Now that I've changed something can I do something
     private Button button;
+    private ListView list;
 }
