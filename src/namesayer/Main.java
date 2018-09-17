e483591c842d418635d0e967133df6a93c22fca6
George-E-M <georgeedwinmcerlean@gmail.com>
2018-09-17 17:52:27 +1200

    base of start menu

--- src/namesayer/Main.java
+++ src/namesayer/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package namesayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("startMenu.fxml"));
+        primaryStage.setTitle("Name Sayer");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
