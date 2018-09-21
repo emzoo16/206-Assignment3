71f43436c486caa8db1b248943044b4d367ceb73
George-E-M <georgeedwinmcerlean@gmail.com>
2018-09-21 15:17:38 +1200

    Added start and workspace creator functionality

--- src/namesayer/WorkSpaceCreatorController.java
+++ src/namesayer/WorkSpaceCreatorController.java
@@ -1,9 +1,11 @@
 package namesayer;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
+import javafx.collections.ObservableSet;
+import javafx.collections.SetChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,23 +13,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.CheckBoxListCell;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
-import java.util.Observable;
 import java.util.ResourceBundle;
 
 public class WorkSpaceCreatorController implements Initializable {
     @FXML
     TextField textField;
     @FXML
-    ListView<String> databaseRecordings;
+    ListView<String> databaseRecordingsView;
     @FXML
-    ListView<String> workspaceRecordings;
+    ListView<String> workspaceRecordingsView;
     @FXML
     Button addButton;
     @FXML
@@ -38,25 +37,47 @@ public class WorkSpaceCreatorController implements Initializable {
     Button continueButton;
     @FXML
     CheckBox randomiseBox;
-    @FXML
-    CheckBox selectAllBox;
 
-    ObservableList<String> databaseList;
-    ObservableList<String> selectedDatabaseItems;
+    DatabaseList databaseList;
+    WorkspaceList workspaceList;
+    ObservableSet<String> selectedDatabaseItems;
+    ObservableSet<String> selectedWorkspaceItems;
 
     @FXML
     private void addToWorkspace() {
-
+        for (String recordingName : selectedDatabaseItems) {
+            Recording recording = databaseList.getRecording(recordingName);
+            workspaceList.add(recordingName, recording);
+            databaseList.remove(recordingName);
+        }
+        selectedDatabaseItems.clear();
+        databaseRecordingsView.setItems(databaseList.getRecordingNames());
+        workspaceRecordingsView.setItems(workspaceList.getRecordingNames());
     }
 
     @FXML
     private void removeFromWorkspace() {
-
+        for (String recordingName : selectedWorkspaceItems) {
+            workspaceList.remove(recordingName);
+            databaseList.add(recordingName);
+        }
+        selectedWorkspaceItems.clear();
+        workspaceRecordingsView.setItems(workspaceList.getRecordingNames());
+        databaseRecordingsView.setItems(databaseList.getRecordingNames());
     }
 
     @FXML
     private void continueToPractice() {
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("workspace.fxml"));
+            Parent createScene = fxmlLoader.load();
+            WorkspaceController controller = fxmlLoader.getController();
+            //controller.setWorkspaceRecordings(workspaceList);
+            Stage stage = (Stage) continueButton.getScene().getWindow();
+            stage.setScene(new Scene(createScene, 700, 500));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -73,39 +94,50 @@ public class WorkSpaceCreatorController implements Initializable {
         }
     }
 
-    /**
-     * Run on start-up
-     * @param location
-     * @param resources
-     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        databaseList = FXCollections.observableArrayList();
-        //Gets the files in an array
-        File folder = new File("Database/");
-        File[] ArrayOfFiles = folder.listFiles();
-        int count = 1;
-        if (ArrayOfFiles.length != 0) {
-            for (File file : ArrayOfFiles) {
-                //Validates the file
-                if (file.isFile() && !file.toString().startsWith("Database/.")) {
-                    String fileString = file.getName();
-                    //Removes extension and codes.
-                    fileString = fileString.substring(fileString.lastIndexOf("_") + 1, fileString.indexOf("."));
-                    String updatedFileString = fileString;
-                    while (databaseList.contains(updatedFileString)) {
-                        updatedFileString = fileString + "(" + count + ")";
-                        count += 1;
+        //Sets up the two list views to work with checkboxes
+        selectedDatabaseItems = FXCollections.observableSet();
+        databaseList = new DatabaseList();
+        databaseRecordingsView.setItems(databaseList.getRecordingNames());
+        databaseRecordingsView.setCellFactory(CheckBoxListCell.forListView(new Callback<String, ObservableValue<Boolean>>() {
+            @Override
+            public ObservableValue<Boolean> call(String item) {
+                BooleanProperty observable = new SimpleBooleanProperty();
+                observable.addListener((obs, wasSelected, isNowSelected) -> {
+                    if(isNowSelected) {
+                        selectedDatabaseItems.add(item);
+                    } else {
+                        selectedDatabaseItems.remove(item);
                     }
-                    count = 1;
-                    databaseList.add(updatedFileString);
-                }
+                });
+                observable.set(selectedDatabaseItems.contains(item));
+                selectedDatabaseItems.addListener((SetChangeListener.Change<? extends String> c) ->
+                        observable.set(selectedDatabaseItems.contains(item)));
+                return observable;
             }
-        }
-        //Allow the selection of multiple items
-        databaseRecordings.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        databaseRecordings.setOnMouseClicked(event ->
-                selectedDatabaseItems = databaseRecordings.getSelectionModel().getSelectedItems());
-        databaseRecordings.setItems(databaseList);
+        }));
+
+        selectedWorkspaceItems = FXCollections.observableSet();
+        workspaceList = new WorkspaceList();
+        workspaceRecordingsView.setItems(workspaceList.getRecordingNames());
+        workspaceRecordingsView.setCellFactory(CheckBoxListCell.forListView(new Callback<String, ObservableValue<Boolean>>() {
+            @Override
+            public ObservableValue<Boolean> call(String item) {
+                BooleanProperty observable = new SimpleBooleanProperty();
+                observable.addListener((obs, wasSelected, isNowSelected) -> {
+                        if(isNowSelected) {
+                            selectedWorkspaceItems.add(item);
+                        } else {
+                            selectedWorkspaceItems.remove(item);
+                        }
+                });
+                observable.set(selectedWorkspaceItems.contains(item));
+                selectedWorkspaceItems.addListener((SetChangeListener.Change<? extends String> c) ->
+                    observable.set(selectedWorkspaceItems.contains(item)));
+
+                return observable;
+            }
+        }));
     }
 }
