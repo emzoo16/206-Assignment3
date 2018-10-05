508729bca5e3d7a4ebcc92c9323b964098a7ca28
emzoo16 <43309131+emzoo16@users.noreply.github.com>
2018-10-06 08:33:41 +1300

    Commented code

--- TestStuffs/Search.java
+++ TestStuffs/Search.java
@@ -24,6 +24,7 @@ ListView<String> listView;
 @Override
 public void initialize(URL location, ResourceBundle resources) {
 
+	//Dummy Data
 	testData.add("Apple");
 	testData.add("Banana");
 	testData.add("Carrot");
@@ -34,15 +35,24 @@ public void initialize(URL location, ResourceBundle resources) {
 	testData.add("Blueberry");
 	testData.add("Grape");
 	
+	//Filtered list that will update list accordingly as use types more characters.
 	filteredList= new FilteredList<>(testData, data -> true);
 	listView.setItems(filteredList);
+	
+	//Binding the filtered list to the searchBar.
 	searchBar.textProperty().addListener(((observable, oldValue, newValue) -> {
 	        filteredList.setPredicate(currentValue -> {
 	        	String lastChar = newValue.substring(newValue.length() - 1);
+	        	
+	        	//Tests if the user has type a space or underscore, ie if the
+	        	//have moved on to another name. Thought we could handle refreshing the
+	        	//Search here.
 	        	if(lastChar.equals(" ") || lastChar.equals("_")) {
+	        		
 	        		filteredList.setPredicate(x -> true);
                 }
 	        	
+	        	//If nothing new has been entered
 	            if (newValue == null || newValue.isEmpty()){
                 return true;
 	            }
@@ -53,25 +63,5 @@ public void initialize(URL location, ResourceBundle resources) {
 	}));
 }
 
-public void addSearchListener() {
-	filteredList= new FilteredList<>(testData, data -> true);
-	listView.setItems(filteredList);
-	searchBar.textProperty().addListener(((observable, oldValue, newValue) -> {
-	        filteredList.setPredicate(currentValue -> {
-	        	String lastChar = newValue.substring(newValue.length() - 1);
-	        	if(lastChar.equals(" ") || lastChar.equals("_")) {
-	        		filteredList = new FilteredList<>(testData, data -> true);
-	        		listView.setItems(filteredList);
-                }
-	        	
-	            if (newValue == null || newValue.isEmpty()){
-                return true;
-	            }
-            String lowerCaseSearch = newValue.toLowerCase();
-            return currentValue.toLowerCase().contains(lowerCaseSearch);
-        });
-
-	}));
-}
 
 }
