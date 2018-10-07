cd58ce78e3bb6cb20b891979b4c9acd885f97d1d
emzoo16 <43309131+emzoo16@users.noreply.github.com>
2018-10-07 18:59:31 +1300

    Commented + updated UI

--- AnotherDuplicatelol/src/namesayer/PlaylistController.java
+++ AnotherDuplicatelol/src/namesayer/PlaylistController.java
@@ -19,6 +19,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -29,13 +30,11 @@ public class PlaylistController implements Initializable {
 	 * FXML variables 
 	 */
 	@FXML
-	Button playButton;
-	@FXML
-	Button nextButton;
-	@FXML
-	Button previousButton;
-	@FXML
 	Button uploadButton;
+	@FXML
+	Button browseButton;
+	@FXML
+	TextField fileText;
 	@FXML
 	Button backButton;
 	@FXML
@@ -46,16 +45,16 @@ public class PlaylistController implements Initializable {
 	ListView<String> nameListView;
 	ObservableList<String> nameList = FXCollections.observableArrayList();
 	
-	/*
-	 * Keeping track of the number in the listViews the user has currently selected
-	 * in the playListView and nameListView respectively.
-	 */
+	
+	 //Keeping track of the number in the listViews the user has currently selected
+	 //in the playListView and nameListView respectively. 
 	int playlistIndex;
 	int nameIndex;
 	
+	//Stores a reference to the file the user chooses from the file chooser.
 	File currentFile;
-	String[] namesNotFound;
 
+	//List of recording objects to be passed to the workspaceController.
 	DatabaseList workspaceRecordings;
 	
 	@Override
@@ -65,6 +64,10 @@ public class PlaylistController implements Initializable {
 		workspaceRecordings = new DatabaseList();
 	}
 
+	/*
+	 * This method is invoked when the user clicks the back button. Takes the user back to the start
+	 * menu.
+	 */
 	@FXML
 	public void backButtonClicked() {
 		try {
@@ -77,13 +80,21 @@ public class PlaylistController implements Initializable {
 		}
 	}
 
+	/*
+	 * This button is invoked when the user is ready to continue to the workspace, where they will play the 
+	 * selected recordings.
+	 */
 	@FXML
 	public void continueButtonClicked() {
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("workspace.fxml"));
 			Parent playlistScene = fxmlLoader.load();
 			WorkSpaceController controller = fxmlLoader.getController();
+			
+			//Passing the selected recordings to the workspace so they can be shown in the listviews in the
+			//workspace.
 			controller.setWorkspaceRecordingsAndController(workspaceRecordings, workspaceRecordings.getRecordingNames());
+			
 			Stage stage = (Stage) continueButton.getScene().getWindow();
 			stage.setScene(new Scene(playlistScene, 700, 500));
 		} catch (IOException e) {
@@ -91,22 +102,44 @@ public class PlaylistController implements Initializable {
 		}
 	}
 
+	/*
+	 * This method brings up a file chooser where the user is prompted to choose a .txt file to upload.
+	 */
+	@FXML
+	public void browseButtonClicked() {
+		//Creating a new FileChooser with a filter so it only accepts .txt files.
+		FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+		fileChooser.getExtensionFilters().add(extFilter);
+		
+		//Get a reference to the selected file.
+		File selectedFile = fileChooser.showOpenDialog(null);
+		
+		//If the user successfully chooses a file, scan the file.
+		if (selectedFile != null) {
+			currentFile = selectedFile;
+			fileText.setText(currentFile.getName());
+		}
+
+	}
+	
+	/*
+	 * This method is invoked when the user chooses to upload the chosen file from the fileChooser.
+	 */
 	@FXML
 	public void uploadButtonClicked() {
-		//Creating a new FileChooser and creating a filter so it only accepts .txt files.
-				FileChooser fileChooser = new FileChooser();
-				FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-				fileChooser.getExtensionFilters().add(extFilter);
-				
-				File selectedFile = fileChooser.showOpenDialog(null);
-				
-				if (selectedFile != null) {
-					currentFile = selectedFile;
-					scanFile();
-				}else {
-					
-				}
-
+		
+		//If no file is chose, show a warning alert.
+		if(currentFile == null) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Warning");
+			alert.setHeaderText(null);
+			alert.setContentText("Please choose a file" );
+			alert.showAndWait();
+			
+		}else {
+			scanFile();
+		}
 	}
 	
 	/*
@@ -118,9 +151,10 @@ public class PlaylistController implements Initializable {
 		
 		//Indexers to keep track of current position in the arrays.
 		int notFoundIndex=0;
-		int foundIndex=0;
+		
+		//An array that will contain all the valid names from the file.
 		List<String> namesList = new ArrayList<>();
-		//
+
 		if (currentFile.exists()) {
 			try {
 				Scanner scanner = new Scanner(currentFile);
@@ -129,9 +163,7 @@ public class PlaylistController implements Initializable {
 				while (scanner.hasNextLine()) {
 					String line = scanner.nextLine();
 					
-					//Two arrays of strings to store the names of the names that have been found and 
-					//have not been found in the database respectively
-					String[] foundFileNames = {""};
+					//An array containing all the names in the current line that could not be found in the database.
 					String[] notFound = {""};
 
 					//Seperate the line by spaces or underscores to create an array of single names.
@@ -142,30 +174,32 @@ public class PlaylistController implements Initializable {
 					for (int i=0; i<splitLine.length; i++ ) {
 						
 						//If the name is not in the database, add the name to the not found list.
-						if (inDatabase(splitLine[i]) == "") {
+						if (!inDatabase(splitLine[i])) {
 							notFound[notFoundIndex] = splitLine[i];
 							
 						//If the name is in the database, add its fully qualified name to the found names
 						//array
-						}else {
-							foundFileNames[foundIndex] = inDatabase(splitLine[i]);
 						}
 					}
 					
 					//If there are names in the notFound list, show a warning to the user to ask them if they want to concatenate
 					//the found names anyway or cancel.
-					if(notFound.length > 0 && notFound[0] != "") {
+					if(notFound[0] != "") {
 						Alert alert = new Alert(AlertType.WARNING);
 						alert.setTitle("Warning");
 						alert.setHeaderText("The following names were not found: ");
 						
+						//Creates an error message telling reader which one of the names was not found.
 						String errorText = "";
 						for(int i=0; i<notFound.length; i++) {
 							errorText=errorText+ notFound[i];
 						}
+						
 						alert.setContentText(errorText + "\n" + "The name "+ line + " was not created." );
 						alert.showAndWait();
 					}else {
+						//If all the names are in the database, add the line (the composite name) to the namesList
+						//Array.
 						namesList.add(line);
 					}
 				}
@@ -205,11 +239,14 @@ public class PlaylistController implements Initializable {
 	 * This method checks if the provided filename is a name in the database directory.
 	 * Returns a string of the full name of the file if it exists. Otherwise return an empty string.
 	 */
-	public String inDatabase(String fileName) {
+	public boolean inDatabase(String fileName) {
+		
+		//List all the files in the database.
 		File database = new File("Database/");
 		File[] databaseFiles = database.listFiles();
-		String fullFileName = "";
 		
+		//Iterate through the database to check if the current filename matches a name in the
+		//database.
 		for (int i=0; i< databaseFiles.length; i++) {
 			
 			//Parsing the actual name from the long file name.
@@ -217,13 +254,12 @@ public class PlaylistController implements Initializable {
 			String shortName = name.substring(name.lastIndexOf("_") + 1).replaceAll(".wav", "");
 			
 			//Checks if the given file name and the current file in the database directory
-			//match.
+			//match. If they are, return true.
 			if (shortName.equals(fileName)) {
-				fullFileName = name;
-				return fullFileName;
+				return true;
 			}
 		}		
-		return fullFileName;
+		return false;
 		
 	}
 	
